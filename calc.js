f83f6c786c948926314386a81b74981cc6fe8d58
KhufuLintalou <148902267+KhufuLintalou@users.noreply.github.com>
2024-06-17 13:03:23 +0700

    Added a function that takes an operator and do math on two numbers.

--- calc.js
+++ calc.js
@@ -14,3 +14,18 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+let firstNumber;
+let operator;
+let secondNumber;
+
+function operate(operator, number1, number2) {
+    if (operator === "+") {
+        return add(number1, number2);
+    } else if (operator === "-") {
+        return subtract(number1, number2);
+    } else if (operator === "*") {
+        return multiply(number1, number2);
+    } else if (operator === "/") {
+        return divide(number1, number2);
+    }
+}
