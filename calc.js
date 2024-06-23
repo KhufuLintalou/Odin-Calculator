923b7c2753409240a955d4b533f66d82dacbe50c
KhufuLintalou <148902267+KhufuLintalou@users.noreply.github.com>
2024-06-23 15:40:42 +0700

    Clicking the digit button now clear previous operation result. Also did some changes to the previous code.

--- calc.js
+++ calc.js
@@ -38,95 +38,175 @@ let clearButton = document.querySelector(".clear");
 let equalButton = document.querySelector(".equal");
 
 let displayValue = document.createElement("span");
-displayValue.textContent = 0;
 display.appendChild(displayValue);
+display.textContent = 0;
+
+let result = "";
 
 buttonContainer.addEventListener("click", function(e) {
     let target = e.target;
     switch (target) {
         case digitButtons[0]:
-          if (displayValue.textContent === 0) {
+          if (display.textContent == 0) {
+            display.textContent = "";
+          }
+          if (result !== "") {
+            display.textContent = "";
             displayValue.textContent = "";
           }
           displayValue.textContent += 0;
+          display.textContent += 0;
+          result = "";
           break;
         case digitButtons[1]:
-          if (displayValue.textContent == 0) {
+          if (display.textContent == 0) {
+            display.textContent = "";
+            result = "";
+          }
+          if (result !== "") {
+            display.textContent = "";
             displayValue.textContent = "";
           }
           displayValue.textContent += 1;
+          display.textContent += 1;
+          result = "";
           break;
         case digitButtons[2]:
-          if (displayValue.textContent == 0) {
+          if (display.textContent == 0) {
+            display.textContent = "";
+          }
+          if (result !== "") {
+            display.textContent = "";
             displayValue.textContent = "";
           }
           displayValue.textContent += 2;
+          display.textContent += 2;
+          result = "";
           break;
         case digitButtons[3]:
-          if (displayValue.textContent == 0) {
+          if (display.textContent == 0) {
+            display.textContent = "";
+          }
+          if (result !== "") {
+            display.textContent = "";
             displayValue.textContent = "";
           }
           displayValue.textContent += 3;
+          display.textContent += 3;
+          result = "";
           break;
         case digitButtons[4]:
-          if (displayValue.textContent == 0) {
+          if (display.textContent == 0) {
+            display.textContent = "";
+          }
+          if (result !== "") {
+            display.textContent = "";
             displayValue.textContent = "";
           }
           displayValue.textContent += 4;
+          display.textContent += 4;
+          result = "";
           break;
         case digitButtons[5]:
-          if (displayValue.textContent == 0) {
+          if (display.textContent == 0) {
+            display.textContent = "";
+          }
+          if (result !== "") {
+            display.textContent = "";
             displayValue.textContent = "";
           }
           displayValue.textContent += 5;
+          display.textContent += 5;
+          result = "";
           break;
         case digitButtons[6]:
-          if (displayValue.textContent == 0) {
+          if (display.textContent == 0) {
+            display.textContent = "";
+          }
+          if (result !== "") {
+            display.textContent = "";
             displayValue.textContent = "";
           }
           displayValue.textContent += 6;
+          display.textContent += 6;
+          result = "";
           break;
         case digitButtons[7]:
-          if (displayValue.textContent == 0) {
+          if (display.textContent == 0) {
+            display.textContent = "";
+          }
+          if (result !== "") {
+            display.textContent = "";
             displayValue.textContent = "";
           }
           displayValue.textContent += 7;
+          display.textContent += 7;
+          result = "";
           break;
         case digitButtons[8]:
-          if (displayValue.textContent == 0) {
+          if (display.textContent == 0) {
+            display.textContent = "";
+          }
+          if (result !== "") {
+            display.textContent = "";
             displayValue.textContent = "";
           }
           displayValue.textContent += 8;
+          display.textContent += 8;
+          result = "";
           break;
         case digitButtons[9]:
-          if (displayValue.textContent == 0) {
+          if (display.textContent == 0) {
+            display.textContent = "";
+          }
+          if (result !== "") {
+            display.textContent = "";
             displayValue.textContent = "";
           }
           displayValue.textContent += 9;
+          display.textContent += 9;
+          result = "";
           break;
         
         case operatorButtons[0]:
           displayValue.textContent += " + ";
+          display.textContent += " + "
           operatorButtons = "";
+          if (!result == "") {
+            result = "";
+          }
           break;
         case operatorButtons[1]:
           displayValue.textContent += " - ";
+          display.textContent += " - ";
           operatorButtons = "";
+          if (!result == "") {
+            result = "";
+          }
           break;
         case operatorButtons[2]:
           displayValue.textContent += " * ";
+          display.textContent += " * ";
           operatorButtons = "";
+          if (!result == "") {
+            result = "";
+          }
           break;
         case operatorButtons[3]:
           displayValue.textContent += " / ";
+          display.textContent += " / ";
           operatorButtons = "";
+          if (!result == "") {
+            result = "";
+          }
           break;
         
         case clearButton:
-          if (displayValue.textContent !== 0) {
-            displayValue.textContent = "";
+          if (display.textContent !== "0") {
+            display.textContent = "";
           }
           displayValue.textContent = 0;
+          display.textContent = 0;
           operatorButtons = document.querySelectorAll(".operator");
           break;
         
@@ -134,9 +214,10 @@ buttonContainer.addEventListener("click", function(e) {
           let firstNum = Number(displayValue.textContent.split(" ").slice(0, 1));
           let expOperator = displayValue.textContent.split(" ").slice(1, 2).toString();
           let secondNum = Number(displayValue.textContent.split(" ").slice(2, 3));
-          if (!firstNum.toString() || secondNum || expOperator == null) {
-            displayValue.textContent = "";
-            displayValue.textContent = operate(expOperator, firstNum, secondNum);
+          if (!firstNum.toString() || secondNum.toString() || expOperator == null) {
+            display.textContent = operate(expOperator, firstNum, secondNum);
+            displayValue.textContent = display.textContent;
+            result = display.textContent;
           }
           operatorButtons = document.querySelectorAll(".operator");
     }
